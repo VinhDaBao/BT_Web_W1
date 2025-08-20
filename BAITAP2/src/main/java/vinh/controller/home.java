fb71c22aaf32f254a429103cd0643c49f0a19ed2
VinhDaBao <trinhduycongvinh@gmail.com>
2025-08-20 23:00:43 +0700

    Bài 2

---
+++ BAITAP2/src/main/java/vinh/controller/home.java
@@ -0,0 +1,56 @@
+package vinh.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class home
+ */
+@WebServlet("/home")
+public class home extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public home() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession(false);
+		PrintWriter out = response.getWriter();
+		if (session != null) {
+			String name = (String) session.getAttribute("name");
+			out.print("Chào bạn, " + name + " đến với trang quản lý tài khoản");
+		} else {
+			out.print("Xin vui lòng đăng nhập");
+			response.sendRedirect("/BAITAP2/login.html");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
